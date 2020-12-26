d53697c76769f3984e5c88de728c5d7eb7bb54bb
xuchunlei3 <xuchunlei3@wanda.cn>
2020-12-27 11:50:10 +1300

    ajust home image with flex

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -16,17 +16,18 @@ class Home extends Component {
                     <h1 className={style.home__title}>Hi,<br/>I'am <span className={style["home__title-color"]}>{home.profileName}</span><br/> Software Engineer</h1>
                     
                 </div>
-                <div className={style.home__social}>
-                    <a href="" className={style["home__social-icon"]}><i class='bx bxs-envelope' ></i></a>
-                    <a href="" className={style["home__social-icon"]}><i class='bx bxs-phone' ></i></a>
-                    <a href="" className={style["home__social-icon"]}><i class='bx bxl-linkedin'></i></a>
-                    <a href="" className={style["home__social-icon"]}><i class='bx bxl-github' ></i></a>
-                </div>
+                <div className={style.home__detail}>
+                    <div className={style.home__social}>
+                        <a href="" className={style["home__social-icon"]}><i class='bx bxs-envelope' ></i></a>
+                        <a href="" className={style["home__social-icon"]}><i class='bx bxs-phone' ></i></a>
+                        <a href="" className={style["home__social-icon"]}><i class='bx bxl-linkedin'></i></a>
+                        <a href="" className={style["home__social-icon"]}><i class='bx bxl-github' ></i></a>
+                    </div>
 
-                <div className={style.home__img}>    
-                    <img src={process.env.PUBLIC_URL + home.profilePic} alt="" />
+                    <div className={style.home__img}>    
+                        <img src={process.env.PUBLIC_URL + home.profilePic} alt="" />
+                    </div>
                 </div>
-                
                 
             </section>
         )
